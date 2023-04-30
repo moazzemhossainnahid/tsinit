0b72506583fe937e91b79aa244e7dcca92bb3da2
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-04-30 22:43:14 +0600

    define types

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,21 @@
-const course:string = "Next Level Web Development";
+const course: string = "Next Level Web Development";
 console.log("Next Level Web Development");
+
+
+const roll: number = 1122;
+
+roll.toFixed(2);
+
+const isAdmin: boolean = true;
+
+let bc:number;
+
+// bc="djgf"
+
+
+// Primitive Types
+// Number
+// Boolean
+// String
+// Null
+// Undefined
