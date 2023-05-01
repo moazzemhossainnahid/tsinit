8c19aed5e2266830e26fef2bf9c49aca0a631d81
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-05-01 20:41:08 +0600

    add object

---
+++ src/array.ts
@@ -0,0 +1,8 @@
+const names: string[] = ['Nahid', 'Abul', 'Babul']
+const rollnumber: number[] = [23, 45, 78]
+
+const nms = ['rohim', 'korim', 89, 45, false];
+
+const user: [number, string] = [567, 'Nahid']
+
+const me: [string, boolean] = ['Abul', false]
---
+++ src/object.ts
@@ -0,0 +1,13 @@
+const user1: {
+    // company: 'Programming Hero',
+    readonly company: string,
+    name: string,
+    age: number,
+    isMarried: boolean,
+    wife?: string
+} = {
+    company: 'Programming Hero',
+    name: "Moazzem Nahid",
+    age: 23,
+    isMarried: false,
+}
